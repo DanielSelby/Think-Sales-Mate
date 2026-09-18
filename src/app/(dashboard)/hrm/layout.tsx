c7b83c29c1e7c8ffdb6825b956294680cdc09824
DanielSelby <daddyselbydatasolution@gmail.com>
2026-09-18 08:42:15 +0000

    ju

--- src/app/(dashboard)/hrm/layout.tsx
+++ src/app/(dashboard)/hrm/layout.tsx
@@ -2,7 +2,7 @@ import { HrmTabNavigation } from "@/components/hrm/hrm-tab-navigation";
 
 export default function HrmLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="space-y-1">
+    <div className="hrm-page space-y-1">
       <HrmTabNavigation />
       {children}
     </div>
